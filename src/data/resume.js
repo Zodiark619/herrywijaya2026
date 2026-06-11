057599affcb8b3d6e264f1d10ea0b9d66b5c9ed0
asd asd <seonghyejin33@gmail.com>
2026-06-11 15:46:23 +0700

    portfolioherrywijaya2026

---
+++ src/data/resume.js
@@ -0,0 +1,74 @@
+export const resume = {
+  name: "Herry Wijaya",
+  address: "Kota Bogor",
+  phone: "0895632067037",
+  email: "herrywijaya.33@gmail.com",
+  overview: `Self-taught Junior .NET Developer dengan pengalaman dalam membuat aplikasi web dengan C#,
+ASP.NET Core, dan SQL Server. Familiar dengan REST APIs, Entity Framework, dan frontend
+technologies HTML, CSS, dan JavaScript menggunakan React dan .NET MVC. Memiliki skill
+problem-solving yang kuat and mampu memberikan kontribusi kepada tim software development.
+Memiliki pengalaman kerja 1 tahun sebagai programmer dan merupakan lulusan S1 Ilmu Komputer
+di Universitas Pakuan.`,
+  linkedin: "https://www.linkedin.com/in/herry-wijaya-78b7a5216/",
+  github: "https://www.github.com/zodiark619",
+
+  workExperience: [
+    {
+      position: "Software Developer",
+      location: "Mede Media Softika - Cilandak, Jakarta Selatan",
+      duration: "April 2025 - Juli 2025",
+      description: `
+• Membuat query report di dalam postgresql berbentuk union yang dibungkus menjadi View
+dan Stored Procedure
+• Design report jaspersoft dan koneksi dengan query report yang dibuat
+• Membuat mvc di backend springboot menggunakan bahasa java dan koneksi dengan query
+report yang dibuat menggunakan repository pattern
+• Design front end dan koneksi dengan backend menggunakan ajax
+• Project yang dikerjakan berbentuk aplikasi akuntansi dapenbi, menggunakan tools github
+dan figma
+      `,
+    },
+    {
+      position: "Test Environment Engineer",
+      location: "BNI SKTI - Palmerah, Jakarta Selatan",
+      description: `
+      • Menyiapkan environment di remote server dan memasang paket deploy dari change request
+yang diterima di server testing agar siap fase UAT. Tugas yang diterima tertulis di dokumen
+delivery yang dibuat oleh tim dev dan sudah dilakukan versioning oleh tim source control
+• Ruang lingkup project yang dikerjakan adalah aplikasi perbankan
+• Memasang API di aplikasi Axway Policy Manager, melakukan konfigurasi IIS, dan
+memasang query perubahan dan backupnya di server database testing di aplikasi SSMS
+      `,
+      duration: "Desember 2023 - Juni 2024",
+    },
+    {
+      position: ".NET Developer",
+      location: "Adicipta Inovasi Teknologi - Palmerah, Jakarta Selatan",
+      description: `
+• Membuat implementasi baru atau modifikasi perubahan proposal change request tentang
+ASP.NET 4 projects menggunakan ASP.NET Web Forms dengan C# dan VB .NET.
+Implementasi tersebut juga menggunakan SQL stored procedure dan Crystal Report’s RPT
+file
+• Ruang lingkup project yang dikerjakan adalah multifinance dan accounting
+• Membuat dokumen user manual sebagai dokumentasi untuk package deployment saat fase
+deployment    
+      `,
+      duration: "Desember 2022 - Juni 2023",
+    },
+  ],
+  education: [
+    {
+      education: "S1 Ilmu Komputer",
+      location: "Universitas Pakuan",
+      grade: "3.78",
+      duration: "2016-2021",
+    },
+  ],
+  skills: ["React", ".NET", "C#", "Javascript", "HTML", "CSS", "SQLServer"],
+  certifications: [
+    {
+      name: "MTA 98-381",
+      link: "https://www.certiport.com/Portal/Pages/PrintTranscriptInfo.aspx?action=Cert&id=395&cvid=42ulf6IJhMO+xXbfuRbtUA==",
+    },
+  ],
+};
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import Resume from "./pages/resume/Resume";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Resume />} />
+      {/* <Route path="/freelancer" element={<Freelancer />} /> */}
+    </Routes>
+  </BrowserRouter>,
+);
