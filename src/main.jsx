1f56d3b6df5e5d1e7c2681edd52cdf0c1e2145df
asd asd <seonghyejin33@gmail.com>
2026-06-11 16:04:28 +0700

    qwq

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { createRoot } from "react-dom/client";
 import Resume from "./pages/resume/Resume";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <Routes>
       <Route path="/" element={<Resume />} />
+
       {/* <Route path="/freelancer" element={<Freelancer />} /> */}
     </Routes>
   </BrowserRouter>,
